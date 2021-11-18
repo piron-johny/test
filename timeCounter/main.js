272ebf24c802d9cf870992f19913b707d08863c8
Piron Evgeniy <85217377+piron-johny@users.noreply.github.com>
2021-11-18 14:11:17 +0200

    2

--- timeCounter/main.js
+++ timeCounter/main.js
@@ -1,5 +1,5 @@
 //finished time
-const dateFinishCounter = new Date("Jan 01, 2022 00:00:00").getTime();
+const dateFinishCounter = new Date("Jan 01, 2022 00:00:01").getTime();
 const alert = document.querySelector('.alert')
 
 //function, 1000 ms
